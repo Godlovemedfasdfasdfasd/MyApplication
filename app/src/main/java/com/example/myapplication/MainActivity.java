d32d03aba52fee14617573c4a4fa04c94183b9f8
Godlovemedfasdfasdfasd <1462954765@qq.com>
2020-03-09 13:15:27 +0800

    v5 jni and call c lib print is ok

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -20,7 +20,6 @@ public class MainActivity extends AppCompatActivity {
     class MyButtonListener implements View.OnClickListener{
         @Override
         public void onClick(View v) {
-            HardControl d = new HardControl();
             ledon =!ledon;
             if(ledon) {
                 button.setText("ALL OFF");
@@ -28,6 +27,8 @@ public class MainActivity extends AppCompatActivity {
                 checkBoxled2.setChecked(true);
                 checkBoxled3.setChecked(true);
                 checkBoxled4.setChecked(true);
+                for(int i= 0;i<4;i++)
+                    HardControl.ledCtrl(i,1);
             }
             else {
                 button.setText("ALL ON");
@@ -35,6 +36,8 @@ public class MainActivity extends AppCompatActivity {
                 checkBoxled2.setChecked(false);
                 checkBoxled3.setChecked(false);
                 checkBoxled4.setChecked(false);
+                for(int i= 0;i<4;i++)
+                    HardControl.ledCtrl(i,0);
             }
         }
     }
@@ -48,33 +51,41 @@ public class MainActivity extends AppCompatActivity {
             case R.id.LED1:
                 if (checked) {
                     Toast.makeText(getApplicationContext(), "LED1 on", Toast.LENGTH_SHORT).show();// 显示时间较短
+                    HardControl.ledCtrl(0, 1);
                 }
                 else{
                     Toast.makeText(getApplicationContext(), "LED1 off", Toast.LENGTH_SHORT).show();// 显示时间较短
+                    HardControl.ledCtrl(0, 0);
                 }
                 break;
             case R.id.LED2:
                 if (checked) {
                     Toast.makeText(getApplicationContext(), "LED2 on", Toast.LENGTH_SHORT).show();// 显示时间较短
+                    HardControl.ledCtrl(1, 1);
                 }
                 else{
                     Toast.makeText(getApplicationContext(), "LED2 off", Toast.LENGTH_SHORT).show();// 显示时间较短
+                    HardControl.ledCtrl(1, 0);
                 }
                 break;
             case R.id.LED3:
                 if (checked) {
                     Toast.makeText(getApplicationContext(), "LED3 on", Toast.LENGTH_SHORT).show();// 显示时间较短
+                    HardControl.ledCtrl(2, 1);
                 }
                 else{
                     Toast.makeText(getApplicationContext(), "LED3 off", Toast.LENGTH_SHORT).show();// 显示时间较短
+                    HardControl.ledCtrl(2, 0);
                 }
                 break;
             case R.id.LED4:
                 if (checked) {
                     Toast.makeText(getApplicationContext(), "LED4 on", Toast.LENGTH_SHORT).show();// 显示时间较短
+                    HardControl.ledCtrl(3, 1);
                 }
                 else{
                     Toast.makeText(getApplicationContext(), "LED4 off", Toast.LENGTH_SHORT).show();// 显示时间较短
+                    HardControl.ledCtrl(3, 0);
                 }
                 break;
             // TODO: Veggie sandwich
@@ -85,9 +96,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
         button = (Button) findViewById(R.id.BUTTON);
 
+        HardControl.ledOpen();  //在入口函数中调用本地方法打开led，为什么可以直接调用呢个，Ctrl+B 跳转到类定义 因为ledopen是static修饰，静态方法不需要实例化对象
         checkBoxled1 = (CheckBox) findViewById(R.id.LED1);
         checkBoxled2 = (CheckBox) findViewById(R.id.LED2);
         checkBoxled3 = (CheckBox) findViewById(R.id.LED3);
